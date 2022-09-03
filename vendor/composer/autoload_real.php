0e516dd05d1c0da3a22fc0ea0c7b2152de4b6685
Arda Webber <tidze.gh@gmail.com>
2022-09-03 10:03:54 +0430

    Php version changed to 8.0.3.

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInitc1d5f861e336661b9e7463e9420b27db
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInitc1d5f861e336661b9e7463e9420b27db', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitc1d5f861e336661b9e7463e9420b27db', 'loadClassLoader'));
